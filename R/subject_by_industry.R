1dfa5bb7f7f413bb2337a2f5a9a1852ff2f1f28c
Hannah Keogan <hannah.keogan@education.gov.uk>
2023-03-14 14:53:19 +0000

    Test making changes to template

--- R/subject_by_industry.R
+++ R/subject_by_industry.R
@@ -6,12 +6,8 @@
 SubByIndPanel <- function() {
   tabPanel(value = "subject by industry", "Subject by industry")
   ## Create a static cross-tab
-  crosstab_test <- data_df %>% 
-    filter(SSATier1 == "Business, Administration and Law", SSATier2 == 'All', Provision == 'All',
-           LevelOfLearning == 'All', AppType == 'All', Gender == 'All', AgeGroup == 'All', Ethnicity == 'All') %>%
-    select(SSATier1, IndustrySection, NumberSustainedEmployment )
-  
-  
-  
-  
-}
+  # crosstab_test <- data_df %>% 
+  #   filter(SSATier1 == "Business, Administration and Law", SSATier2 == 'All', Provision == 'All',
+  #          LevelOfLearning == 'All', AppType == 'All', Gender == 'All', AgeGroup == 'All', Ethnicity == 'All') %>%
+  #   select(SSATier1, IndustrySection, NumberSustainedEmployment )
+  }
