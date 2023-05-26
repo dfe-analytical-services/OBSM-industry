955e4868ac34c139cdd1b984fa7e22444b58181c
KEOGAN <Hannah.KEOGAN@EDUCATION.GOV.UK>
2023-05-26 10:01:35 +0100

    Remove template code

--- R/read_data.R
+++ R/read_data.R
@@ -11,22 +11,6 @@
 # to either add the file to .gitignore or add an entry for the file into
 # datafiles_log.csv.
 
-read_revenue_data <- function(file = "data/la_maintained_schools_revenue_reserve_final.csv") {
-  # This reads in an example file. For the purposes of this demo, we're using the
-  # latest LA expenditure data downloaded from the EES release.
-  dfRevenue <- read.csv(file)
-  # The time period column name has some non-ascii characters so we're just going to rename it here.
-  colnames(dfRevenue)[1] <- "time_period"
-  dfRevenue <- dfRevenue %>% mutate(
-    year = as.numeric(paste0("20", substr(format(time_period), 5, 6))),
-    area_name = case_when(
-      geographic_level == "National" ~ country_name,
-      geographic_level == "Regional" ~ region_name,
-      TRUE ~ la_name
-    )
-  )
-  return(dfRevenue)
-}
 
 read_ind_data <- function(file = "data/OBSM_industry.csv") {
   dfInd <- read.csv(file)
