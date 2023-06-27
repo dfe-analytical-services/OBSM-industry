683931ac5f905d7e6ccb37c5cc05eaf8329a83af
Rich Bielby <richard.bielby@education.gov.uk>
2023-06-27 09:54:30 +0100

    Updated accessibility statement

--- R/standard_panels.R
+++ R/standard_panels.R
@@ -11,12 +11,11 @@ a11y_panel <- function() {
             and have actively developed this application with accessibilty in mind."),
           h2("WCAG 2.1 compliance"),
           br("We follow the reccomendations of the ", a(style = "color:#007fb0", href = "https://www.w3.org/TR/WCAG21/", "WCAG 2.1 requirements. ", onclick = "ga('send', 'event', 'click', 'link', 'IKnow', 1)"), "This application has been checked using the ", a(style = "color:#007fb0", href = "https://github.com/ewenme/shinya11y", "Shinya11y tool "), ", which did not detect accessibility issues.
-             Each page in this application has been audited for accessiblity with the page-snapshot functionality in the ", a(style = "color:#007fb0", href = "https://developers.google.com/web/tools/lighthouse", "Google Developer Lighthouse tool"), ". Primarily due to the current limitations of the R packages used to create this application, it does not pass on the following issues:"),
-          tags$div(tags$ul(
-            tags$li("some elements fail to have the appropriate aria tags"),
-            tags$li("some table header ids are not assigned correctly"),
-            tags$li("some image elements do not have an alt attributes (note that where this is the case, those images are primarily for presentation )")
-          )),
+             Each page in this application has been audited for accessiblity with the page-snapshot functionality in the ", 
+             a(style = "color:#007fb0", href = "https://developers.google.com/web/tools/lighthouse", "Google Developer Lighthouse tool"), 
+             ". Primarily due to the current limitations of the R packages used to create this application, it does not fully pass the accessibility auditing. ", 
+             "The reasons for this are outlined in the limitations section below.",
+             "This app does however follow the following guidelines:"),
           tags$div(tags$ul(
             tags$li("uses colours that have sufficient contrast"),
             tags$li("allows you to zoom in up to 300% without the text spilling off the screen"),
@@ -24,16 +23,14 @@ a11y_panel <- function() {
           )),
           h2("Limitations"),
           br(
-            "We recognise that there are still potential issues with accessibility in this application, but we will continue
+            "We recognise that there are still issues with accessibility in this application, but we will continue
              to review updates to technology available to us to keep improving accessibility for all of our users." # For example, these
-            # are known issues that we will continue to monitor and improve:
           ),
-          # tags$div(tags$ul(
-          #   tags$li("List"),
-          #   tags$li("known"),
-          #   tags$li("limitations, e.g."),
-          #   tags$li("Alternative text in interactive charts is limited to titles and could be more descriptive (although this data is available in csv format)")
-          # )),
+          tags$div(tags$ul(
+            tags$li("some elements fail to have the appropriate aria tags"),
+            tags$li("some table header ids are not assigned correctly"),
+            tags$li("some image elements do not have an alt attributes (note that where this is the case, those images are primarily for presentation )")
+          )),
           h2("Feedback"),
           br(
             "If you have any feedback on how we could further improve the accessibility of this application, please contact us at",
