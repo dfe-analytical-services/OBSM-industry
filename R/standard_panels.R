d684923be744259b687a494f33a9a9b65796f162
Rich Bielby <richard.bielby@education.gov.uk>
2023-06-27 11:00:04 +0100

    tidy code

--- R/standard_panels.R
+++ R/standard_panels.R
@@ -10,12 +10,14 @@ a11y_panel <- function() {
             This application is run by the Department for Education. We want as many people as possible to be able to use this application,
             and have actively developed this application with accessibilty in mind."),
           h2("WCAG 2.1 compliance"),
-          br("We follow the reccomendations of the ", a(style = "color:#007fb0", href = "https://www.w3.org/TR/WCAG21/", "WCAG 2.1 requirements. ", onclick = "ga('send', 'event', 'click', 'link', 'IKnow', 1)"), "This application has been checked using the ", a(style = "color:#007fb0", href = "https://github.com/ewenme/shinya11y", "Shinya11y tool "), ", which did not detect accessibility issues.
-             Each page in this application has been audited for accessiblity with the page-snapshot functionality in the ", 
-             a(style = "color:#007fb0", href = "https://developers.google.com/web/tools/lighthouse", "Google Developer Lighthouse tool"), 
-             ". Primarily due to the current limitations of the R packages used to create this application, it does not fully pass the accessibility auditing. ", 
-             "The reasons for this are outlined in the limitations section below.",
-             "This app does however follow the following guidelines:"),
+          br(
+            "We follow the reccomendations of the ", a(style = "color:#007fb0", href = "https://www.w3.org/TR/WCAG21/", "WCAG 2.1 requirements. ", onclick = "ga('send', 'event', 'click', 'link', 'IKnow', 1)"), "This application has been checked using the ", a(style = "color:#007fb0", href = "https://github.com/ewenme/shinya11y", "Shinya11y tool "), ", which did not detect accessibility issues.
+             Each page in this application has been audited for accessiblity with the page-snapshot functionality in the ",
+            a(style = "color:#007fb0", href = "https://developers.google.com/web/tools/lighthouse", "Google Developer Lighthouse tool"),
+            ". Primarily due to the current limitations of the R packages used to create this application, it does not fully pass the accessibility auditing. ",
+            "The reasons for this are outlined in the limitations section below.",
+            "This app does however follow the following guidelines:"
+          ),
           tags$div(tags$ul(
             tags$li("uses colours that have sufficient contrast"),
             tags$li("allows you to zoom in up to 300% without the text spilling off the screen"),
